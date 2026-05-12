6ef2237652a88a9acaedf3ed36c1eab3ba928dd7
Ondřej Ilek <7ondik2007@gmail.com>
2026-05-12 06:52:55 +0200

    mhm

--- vytvorit.php
+++ vytvorit.php
@@ -1,4 +1,4 @@
-<?php
+<!-- <?php
 session_start();
  if (!isset($_SESSION["user_id"])) {
     header("Location: login.html");
@@ -7,4 +7,33 @@ session_start();
 
 
 
+?> -->
+
+
+<?php
+session_start();
+
+
+$correct_username = "admin";
+$correct_password = "1234";
+
+$error = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+
+    if ($username == $correct_username && $password == $correct_password) {
+
+        $_SESSION["loggedin"] = true;
+        $_SESSION["username"] = $username;
+
+        header("Location: dashboard.php");
+        exit();
+
+    } else {
+        $error = "Špatné jméno nebo heslo!";
+    }
+}
 ?>
