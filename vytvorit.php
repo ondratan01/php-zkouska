7fa374d0f69e1ba1d6555c946c5aeb446563f2a6
[Ondra Ilek] <[valid-email]>
2026-05-18 15:18:41 +0200

    mhm

--- vytvorit.php
+++ vytvorit.php
@@ -2,14 +2,14 @@
 $correct_username = "admin";
 $correct_password = "admin";
 $error = "";
-$logged_in = false; // dočasné přihlášení jen pro aktuální request
+$logged_in = false;
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $username = $_POST["username"];
     $password = $_POST["password"];
 
     if ($username === $correct_username && $password === $correct_password) {
-        $logged_in = true; // dočasně přihlášený
+        $logged_in = true;
     } else {
         $error = "Špatné jméno nebo heslo!";
     }
